4b87beb44f78aed7062f9539f697f212a1ff4c0a
Juano <morenojuanjose99@gmail.com>
2020-09-06 02:06:01 -0300

    cree el archivo package.json y instale express. empece el archivo app.js para los ruteos y agregue los links en el index.html para reemplazar temporariamente el menu y poder ir probando la navegacion

---
+++ site/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+ });
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/home.html")
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/login.html")
+});
+
+app.get("/producto", (req, res) => {
+    res.sendFile(__dirname + "/producto.html")
+});
+
+app.get("/sobre_nosotros", (req, res) => {
+    res.sendFile(__dirname + "/sobre_nosotros.html")
+});
+
+app.get("/faq", (req, res) => {
+    res.sendFile(__dirname + "/faq.html")
+});
+
+app.listen(3000, () => console.log("Servidor corriendo"))
