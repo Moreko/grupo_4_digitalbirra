b7554546c2597bab0484e9ff218aa05a19bad3c9
rodtob <rodrigo.toblli@gmail.com>
2020-10-14 00:29:43 -0300

    comienzo filtroHome productos

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const productsFilePath = path.join(__dirname, '../data/db.json')
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 module.exports = {
     index:(req,res)=>{
         res.render('index')
@@ -7,6 +15,16 @@ module.exports = {
     },
     sobre_nosotros:(req,res)=>{
         res.render('sobre_nosotros')
+    },
+
+    filtroHome: (req,res)=>{
+        let infoUsarioCerveza = req.body;
+        if(typeof req.body.tipoCerveza != 'undefined'){
+            if(req.body.eleccion == 'mas-vendidas'){
+                let masVendidas = products.filter(element => element.categoria == req.body.eleccion);
+                res.render('index', {cervezas: masVendidas})
+            }
+        }
     }
 }
 
--- site/src/routes/indexRouter.js
+++ site/src/routes/indexRouter.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 const indexController = require("../controllers/indexController")
 
 /* GET home page. */
 
 router.get('/', indexController.index);
+
+router.post('/',indexController.filtroHome)
 
 router.get('/faq', indexController.faq);
 
