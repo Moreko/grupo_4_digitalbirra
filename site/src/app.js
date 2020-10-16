9500002769bb2dd2524e405f1dafcd182bc73562
Juano <morenojuanjose99@gmail.com>
2020-10-16 01:00:28 -0300

    hice logica de registro usuario

--- site/src/app.js
+++ site/src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session')
+var bcrypt = require("bcryptjs")
 
 var indexRouter = require('./routes/indexRouter');
 let preindexRouter = require('./routes/preindexRouter');
--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+var bcrypt = require("bcryptjs")
+
 const {validationResult} = require('express-validator')
+
+const usersFilePath = path.join(__dirname, '../data/dbUsers.json')
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 module.exports = {
     registro:(req,res)=>{
@@ -8,8 +15,30 @@ module.exports = {
 
         let errors = validationResult(req)
         let oldValues = req.body
+
         if (errors.isEmpty()){
-            res.send('todo bien')
+            
+            // Creo usuario con contraseña encriptada
+            let nuevoUsuario = {
+                "id": users[users.length-1].id+1,
+                "nombre": req.body.regNombre,
+                "apellido": req.body.regApellido,
+                "email": req.body.regMail,
+                "password": bcrypt.hashSync(req.body.regPassword, 10)
+            }
+
+            // Creo variable con base de datos vieja y le sumo el usuario nuevo
+            let newDB = [...users, nuevoUsuario]
+
+            // Lo hago Json
+            newDBJson = JSON.stringify(newDB, null, 2);
+
+            // Lo escribo en el json de base de datos
+            fs.writeFileSync(usersFilePath, newDBJson)
+
+            res.render("registroExitoso")
+
+
         } else{
             res.render("registro", {registerErrors:errors.errors, oldValues})
         }
