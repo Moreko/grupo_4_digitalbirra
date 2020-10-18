20eed5de60ac4dd71b1fd265d20a08423edb6577
juanism31 <juanism31@gmail.com>
2020-10-17 22:52:19 -0300

    subo logica de perfil, me falta todavia

--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -3,6 +3,7 @@ const path = require('path');
 var bcrypt = require("bcryptjs")
 
 const {value, validationResult} = require('express-validator');
+const { isUndefined } = require('util');
 
 const usersFilePath = path.join(__dirname, '../data/dbUsers.json')
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -18,7 +19,17 @@ module.exports = {
 
         // Si no tengo errores, registro, sino, mando vista de registro con errores
         if (errors.isEmpty()){
+            //modificar usuario
             
+            // if(typeof usuarioActivo.nombre!="undefined"){
+            //     let UserModif= users.find(element=>element.nombre==usuarioActivo.nombre)
+            //     let Modif={...UserModif, ...req.body}
+            //     let elIndex=user.indexOf(UserModif)
+            //     users[elIndex]=Modif
+            //     fs.writeFileSync(usersFilePath, JSON.stringify(users, null,2))
+            //     console.log(usuarioActivo)
+
+            // }else{
             // Creo usuario con contraseña encriptada
             let nuevoUsuario = {
                 "id": users[users.length-1].id+1,
@@ -38,11 +49,12 @@ module.exports = {
             fs.writeFileSync(usersFilePath, newDBJson)
 
             res.render("registroExitoso")
-
+            // }
 
         } else{
             res.render("registro", {registerErrors:errors.errors, oldValues})
         }
+    
     },
 
     login:(req,res)=>{
