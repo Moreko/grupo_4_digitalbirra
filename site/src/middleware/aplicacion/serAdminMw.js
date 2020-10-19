9990af24640bcf3c95d9948d39c0761e39d7c577
rodtob <rodrigo.toblli@gmail.com>
2020-10-18 21:33:04 -0300

    middleware aplicacion admin

--- site/src/middleware/aplicacion/serAdminMw.js
+++ site/src/middleware/aplicacion/serAdminMw.js
@@ -1,13 +1,13 @@
 module.exports = function(req, res, next){
-    // // Logica para que chequee si hay administrador
+    // Logica para que chequee si hay administrador
 
-    // // Por defecto, no hay administrador
-    // res.locals.usuarioAdmin = false;
+    // Por defecto, no hay administrador
+    res.locals.usuarioAdmin = false;
 
-    // // Si existe en session el usuario, lo mando a locals para compartirlo con las vistas
-    // if(req.session.admin) {
-    //     res.locals.usuarioAdmin = req.session.admin
-    // } 
+    // Si existe en session el usuario, lo mando a locals para compartirlo con las vistas
+    if(req.session.admin) {
+        res.locals.usuarioAdmin = req.session.admin
+    } 
 
     next();
 
