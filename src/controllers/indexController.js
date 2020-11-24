c54e34ccef3657a165ec9392929af12b06153091
rodtob <rodrigo.toblli@gmail.com>
2020-11-24 09:56:05 -0300

    relinkeo a db heroku

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -11,13 +11,13 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 module.exports = {
     index: async (req,res)=>{
-        // const birras =  await db.Beers.findAll()
-        // res.json(birras)
-        if(req.cookies.serMayor == undefined){
-            res.render('preindex')
-        } else{
-            res.render('index', { cervezas: products.slice(1,4) ,eleccion: 'DIGITAL BIRRA'})
-        }
+        const birras =  await db.Beers.findAll()
+        res.json(birras)
+        // if(req.cookies.serMayor == undefined){
+        //     res.render('preindex')
+        // } else{
+        //     res.render('index', { cervezas: products.slice(1,4) ,eleccion: 'DIGITAL BIRRA'})
+        // }
     },
     faq:(req,res)=>{
         res.render("faq")
