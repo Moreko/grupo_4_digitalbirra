d424ab18a043aa82419565851f5b0e6ba40b0537
juanism31 <juanism31@gmail.com>
2020-12-02 22:15:52 -0300

    sume admin en los renders del filtrohome

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -30,23 +30,24 @@ module.exports = {
     },
 
     filtroHome:async(req,res)=>{
+        let admin = req.session.admin
         const estilos =  await db.Estilos.findAll()
             if(req.body.eleccion == 'mas-vendidas'){
                 let cervezas= await db.Beers.findAll({where:{categoria: 'mas-vendidas'}}, {include:{all:true}})
                 // let masVendidas = products.filter(element => element.categoria == req.body.eleccion);
                 // console.log(masVendidas)
-                 res.render('index', {cervezas, eleccion: req.body.eleccion,estilos})
+                 res.render('index', {admin, cervezas, eleccion: req.body.eleccion,estilos})
             } else if (req.body.eleccion == 'todas'){
                 let cervezas= await db.Beers.findAll()
 
-                res.render('index', {cervezas, eleccion: req.body.eleccion,estilos})
+                res.render('index', {admin,cervezas, eleccion: req.body.eleccion,estilos})
             }
 
             if(req.body.estilo_id != undefined){
                 let tipoCerveza = req.body.estilo_id
                 let cervezas= await db.Beers.findAll({where:{estilo_id: tipoCerveza}}, {include:{all:true}})
                 let eleccion = await db.Estilos.findByPk(tipoCerveza)
-                res.render('index', {cervezas, eleccion: eleccion.nombre, estilos})
+                res.render('index', {admin, cervezas, eleccion: eleccion.nombre, estilos})
             }
 
         }
