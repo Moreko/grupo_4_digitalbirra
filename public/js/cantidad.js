e2bacb5e2fef7a88e0a9b9bc4ead1bf21c4e20aa
rodtob <rodrigo.toblli@gmail.com>
2021-01-14 15:41:27 -0300

    cambio puerto a 5000

---
+++ public/js/cantidad.js
@@ -0,0 +1,7 @@
+const sumarButton = document.querySelector('.agregar')
+
+document.querySelectorAll('.agregar').forEach(item => {
+    item.addEventListener('click', event => {
+      alert('hola')
+    })
+  })
