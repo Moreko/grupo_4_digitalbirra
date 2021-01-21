d358aeef233b484faba15c2e9d6b29457d87583a
juanism31 <juanism31@gmail.com>
2021-01-21 13:40:18 -0300

    botones de sumar y restar

--- public/js/cantidad.js
+++ public/js/cantidad.js
@@ -2,12 +2,16 @@ const sumarButton = document.querySelector('.agregar')
 
 document.querySelectorAll('.agregar').forEach(item => {
     item.addEventListener('click', event => {
-      alert('hola')
+     const sumar= item.parentElement
+    sumar.querySelector('input').stepUp()
+     
+
     })
   })
 
   document.querySelectorAll('.menos').forEach(item => {
     item.addEventListener('click', event => {
-      alert('adios')
+      const restar = item.parentElement
+      restar.querySelector('input').stepDown()
     })
   })
