45925150e193b8d423664db5949b0fd166bfb879
juanism31 <juanism31@gmail.com>
2021-01-22 11:54:19 -0300

    subo botones de mas y menos

--- public/js/cantidad.js
+++ public/js/cantidad.js
@@ -2,24 +2,6 @@ const minusButton = document.getElementById('menos');
 const plusButton = document.getElementById('mas');
 const inputField = document.getElementById('input');
 
-<<<<<<< HEAD
-minusButton.addEventListener('click', event => {
-  event.preventDefault();
-  const currentValue = Number(inputField.value);
-  if (currentValue >1){
-    inputField.value = currentValue - 1;
-  }
-});
-
-plusButton.addEventListener('click', event => {
-  event.preventDefault();
-  const currentValue = Number(inputField.value);
-  if (currentValue < 20){
-
-    inputField.value = currentValue + 1;
-  }
-});
-=======
 document.querySelectorAll('.agregar').forEach(item => {
     item.addEventListener('click', event => {
      const sumar= item.parentElement
@@ -35,4 +17,3 @@ document.querySelectorAll('.agregar').forEach(item => {
       restar.querySelector('input').stepDown()
     })
   })
->>>>>>> 4f72dfeae087e03037ddaa7dd75c41c5c60373e4
