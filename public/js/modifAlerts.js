618609fd2a07b8c2f605fb952891a9fcc77eb934
rodtob <rodrigo.toblli@gmail.com>
2021-01-25 11:59:03 -0300

    alert modificar producto

--- public/js/modifAlerts.js
+++ public/js/modifAlerts.js
@@ -4,10 +4,9 @@ function submitForm(form) {
 }
 
 const eliminarBirra = document.getElementById('eliminar')
-
+const modifBirra = document.getElementById('modificar')
 
 eliminarBirra.addEventListener('click', (e)=>{
-    console.log('blabla')
     e.preventDefault()
     Swal.fire({
         title: 'Are you sure?',
@@ -23,4 +22,22 @@ eliminarBirra.addEventListener('click', (e)=>{
          submitForm(elForm)
         }
       })
+})
+
+modifBirra.addEventListener('click', (e)=>{
+  e.preventDefault()
+  Swal.fire({
+      title: 'Estas seguro de que queres modificar?',
+      text: "You won't be able to revert this!",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, desde ya!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+       const elForm = document.getElementById('elFormModif')
+       submitForm(elForm)
+      }
+    })
 })
